1d66274b0b5cce29863ddc3a99ee06bae207c2e1
Tomasz <tomasz.iwanicki@onet.pl>
2023-03-02 14:43:20 +0100

    Initial commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
-import Home from './Home/Home'
 import Navbar from './Navbar/Navbar'
+import Home from './Home/Home'
+import Destination from './Destination/Destination'
+import Crew from './Crew/Crew'
+import Technology from './Technology/Technology'
 
-export { Home, Navbar }
+export { Home, Navbar, Destination, Crew, Technology }
