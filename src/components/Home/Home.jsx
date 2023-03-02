21489f6f4b7dd489311c98e85d15a67b3bbd88b1
Tomasz <tomasz.iwanicki@onet.pl>
2023-03-02 17:42:07 +0100

    Complete Home section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 import './Home.css'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <p className="home-header">So, you want to travel to</p>
+        <h1>space</h1>
+        <p className="home-description">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+      </div>
+      <div className="home-button">
+        <button>Explore!</button>
+      </div>
+    </div>
+  )
 }
 
 export default Home
