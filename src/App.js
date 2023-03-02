272914db3fe86805e4810263c4b9fa0e6d2b1338
Tomasz <tomasz.iwanicki@onet.pl>
2023-03-02 14:44:15 +0100

    Add some routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Home, Navbar } from './components'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { images } from './constants'
+import { Destination, Home, Navbar, Crew, Technology } from './components'
 
 const App = () => {
   return (
-    <div>
+    <div className="app-container" style={{ backgroundImage: `url(${images.bgHomeDesktop})` }}>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/technology" element={<Technology />} />
+      </Routes>
     </div>
   )
 }
