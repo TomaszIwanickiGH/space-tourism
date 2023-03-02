e0236ea60e7beebf11e7e0dc887a1fb4ebc48cae
Tomasz <tomasz.iwanicki@onet.pl>
2023-03-02 14:44:02 +0100

    Create navbar using routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 import { images } from '../../constants'
 
 const Navbar = () => {
   return (
-    <div>
-      <img src={images.logo} alt="" />
+    <div className="navbar-container">
+      <div className="logo">
+        <Link to="/">
+          <img src={images.logo} alt="logo" />
+        </Link>
+      </div>
+      <div className="navbar-content">
+        <div className="divider"></div>
+        <div className="navbar">
+          <ul>
+            <Link to="/">
+              <li>00 Home</li>
+            </Link>
+            <Link to="/destination">
+              <li>01 Destination</li>
+            </Link>
+            <Link to="/crew">
+              <li>02 Crew</li>
+            </Link>
+            <Link to="/technology">
+              <li>03 Technology</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
