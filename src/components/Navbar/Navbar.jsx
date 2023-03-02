3ff6104922d24daf3da8fbb77d125b0bb584e706
Tomasz <tomasz.iwanicki@onet.pl>
2023-03-02 10:20:55 +0100

    Initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+import { images } from '../../constants'
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={images.logo} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/constants/images.js
@@ -0,0 +1,61 @@
+import bgCrewDesktop from '../assets/crew/background-crew-desktop.jpg'
+import bgCrewMobile from '../assets/crew/background-crew-mobile.jpg'
+import bgCrewTablet from '../assets/crew/background-crew-tablet.jpg'
+import anousheh from '../assets/crew/image-anousheh-ansari.png'
+import douglas from '../assets/crew/image-douglas-hurley.png'
+import mark from '../assets/crew/image-mark-shuttleworth.png'
+import victor from '../assets/crew/image-victor-glover.png'
+import bgDestinDesktop from '../assets/destination/background-destination-desktop.jpg'
+import bgDestinMobile from '../assets/destination/background-destination-mobile.jpg'
+import bgDestinTablet from '../assets/destination/background-destination-tablet.jpg'
+import europa from '../assets/destination/image-europa.png'
+import mars from '../assets/destination/image-mars.png'
+import moon from '../assets/destination/image-moon.png'
+import titan from '../assets/destination/image-titan.png'
+import bgHomeDesktop from '../assets/home/background-home-desktop.jpg'
+import bgHomeMobile from '../assets/home/background-home-mobile.jpg'
+import bgHomeTablet from '../assets/home/background-home-tablet.jpg'
+import bgTechDesktop from '../assets/technology/background-technology-desktop.jpg'
+import bgTechMobile from '../assets/technology/background-technology-mobile.jpg'
+import bgTechTablet from '../assets/technology/background-technology-tablet.jpg'
+import launchLandscape from '../assets/technology/image-launch-vehicle-landscape.jpg'
+import launchPortrait from '../assets/technology/image-launch-vehicle-portrait.jpg'
+import spaceLandscape from '../assets/technology/image-space-capsule-landscape.jpg'
+import spacePortrait from '../assets/technology/image-space-capsule-portrait.jpg'
+import spaceportLandscape from '../assets/technology/image-spaceport-landscape.jpg'
+import spaceportPortrait from '../assets/technology/image-spaceport-portrait.jpg'
+import iconClose from '../assets/shared/icon-close.svg'
+import iconHamburger from '../assets/shared/icon-hamburger.svg'
+import logo from '../assets/shared/logo.svg'
+
+export default {
+  bgCrewDesktop,
+  bgCrewMobile,
+  bgCrewTablet,
+  anousheh,
+  douglas,
+  mark,
+  victor,
+  bgDestinDesktop,
+  bgDestinMobile,
+  bgDestinTablet,
+  europa,
+  mars,
+  moon,
+  titan,
+  bgHomeDesktop,
+  bgHomeMobile,
+  bgHomeTablet,
+  bgTechDesktop,
+  bgTechMobile,
+  bgTechTablet,
+  launchLandscape,
+  launchPortrait,
+  spaceLandscape,
+  spacePortrait,
+  spaceportLandscape,
+  spaceportPortrait,
+  iconClose,
+  iconHamburger,
+  logo,
+}
