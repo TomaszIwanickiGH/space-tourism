7152724c77fc3c0d3867598e5ce48978745bf7ff
Tomasz <tomasz.iwanicki@onet.pl>
2023-03-06 17:14:55 +0100

    Try to change background on navbar click

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { images } from './constants'
 import { Destination, Home, Navbar, Crew, Technology } from './components'
 
 const App = () => {
+  const [background, setBackground] = useState(images.bgHomeDesktop)
+
+  useEffect(() => {
+    console.log('siema')
+  }, [background])
+
+  const getData = (bgImage) => {
+    setBackground(bgImage)
+  }
+
   return (
-    <div className="app-container" style={{ backgroundImage: `url(${images.bgHomeDesktop})` }}>
-      <Navbar />
+    <div className="app-container" style={{ backgroundImage: `url(${background})` }}>
+      <Navbar onClick={getData} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/destination" element={<Destination />} />
