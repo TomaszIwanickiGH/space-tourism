4a9ed53813e3178a0fdac2c09d09e5de97eb4431
Tomasz <tomasz.iwanicki@onet.pl>
2023-03-06 18:20:41 +0100

    Fix navbar so that images work properly

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,71 +9,57 @@ const Navbar = (props) => {
   const [bgImage, setbgImage] = useState(images.bgHomeDesktop)
 
   return (
-    <div className="navbar-container">
+    <div className="navbar-container" {...props.onClick(bgImage)}>
       <div className="logo">
-        <Link to="/">
-          <img
-            src={images.logo}
-            alt="logo"
-            onClick={() => {
-              setActive(1)
-              setbgImage(images.bgHomeDesktop)
-              props.onClick(bgImage)
-            }}
-          />
+        <Link
+          to="/"
+          onClick={() => {
+            setActive(1)
+            setbgImage(images.bgHomeDesktop)
+          }}
+        >
+          <img src={images.logo} alt="logo" />
         </Link>
       </div>
       <div className="navbar-content">
         <div className="divider"></div>
         <div className="navbar">
           <ul>
-            <Link to="/">
-              <li
-                style={active === 1 ? { borderBottom: '2px solid white' } : {}}
-                onClick={() => {
-                  setActive(1)
-                  setbgImage(images.bgHomeDesktop)
-                  props.onClick(bgImage)
-                }}
-              >
-                00 Home
-              </li>
+            <Link
+              to="/"
+              onClick={() => {
+                setActive(1)
+                setbgImage(images.bgHomeDesktop)
+              }}
+            >
+              <li style={active === 1 ? { borderBottom: '2px solid white' } : {}}>00 Home</li>
             </Link>
-            <Link to="/destination">
-              <li
-                style={active === 2 ? { borderBottom: '2px solid white' } : {}}
-                onClick={() => {
-                  setActive(2)
-                  setbgImage(images.bgDestinDesktop)
-                  props.onClick(bgImage)
-                }}
-              >
-                01 Destination
-              </li>
+            <Link
+              to="/destination"
+              onClick={() => {
+                setActive(2)
+                setbgImage(images.bgDestinDesktop)
+              }}
+            >
+              <li style={active === 2 ? { borderBottom: '2px solid white' } : {}}>01 Destination</li>
             </Link>
-            <Link to="/crew">
-              <li
-                style={active === 3 ? { borderBottom: '2px solid white' } : {}}
-                onClick={() => {
-                  setActive(3)
-                  setbgImage(images.bgCrewDesktop)
-                  props.onClick(bgImage)
-                }}
-              >
-                02 Crew
-              </li>
+            <Link
+              to="/crew"
+              onClick={() => {
+                setActive(3)
+                setbgImage(images.bgCrewDesktop)
+              }}
+            >
+              <li style={active === 3 ? { borderBottom: '2px solid white' } : {}}>02 Crew</li>
             </Link>
-            <Link to="/technology">
-              <li
-                style={active === 4 ? { borderBottom: '2px solid white' } : {}}
-                onClick={() => {
-                  setActive(4)
-                  setbgImage(images.bgTechDesktop)
-                  props.onClick(bgImage)
-                }}
-              >
-                03 Technology
-              </li>
+            <Link
+              to="/technology"
+              onClick={() => {
+                setActive(4)
+                setbgImage(images.bgTechDesktop)
+              }}
+            >
+              <li style={active === 4 ? { borderBottom: '2px solid white' } : {}}>03 Technology</li>
             </Link>
           </ul>
         </div>
